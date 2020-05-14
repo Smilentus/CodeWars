c331b7f0df42825bdae768cb291f58c54a32678f
Dmitry <46642049+Smilentus@users.noreply.github.com>
2020-05-14 18:25:38 +0500

    Delete morse.py

--- morse.py
+++
@@ -1,9 +0,0 @@
-def decodeBitsAdvanced(bits):
-    # ToDo: Accept 0's and 1's, return dots, dashes and spaces
-    return bits.replace('111', '-').replace('000', ' ').replace('1', '.').replace('0', '')
-
-def decodeMorse(morseCode):
-    # ToDo: Accept dots, dashes and spaces, return human-readable message
-    return morseCode.replace('.', 'E').replace('-', 'T').replace(' ', '')
-
-print(decodeMorse(decodeBitsAdvanced('0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000')))
